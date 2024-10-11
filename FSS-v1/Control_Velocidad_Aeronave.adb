a5b1c095615ded21cab19a65de6f109a51bb2c38
Nicolas Clavo Collado <nicolas.clavo@gmail.com>
2024-10-11 10:47:49 +0200

    actualizacion de Control_Velocidad_Aeronave con la cabecera de los tasks

---
+++ FSS-v1/Control_Velocidad_Aeronave.adb
@@ -0,0 +1,30 @@
+with Ada.Real_Time; use Ada.Real_Time;
+with devicesFSS_V1; use devicesFSS_V1;
+
+package body Control_Velocidad_Aeronave is
+
+task body Transferir_Potencia_Piloto
+
+end Transferir_Potencia_Piloto
+
+task body Control_Velocidad_Cabeceo
+
+end Control_Velocidad_Cabeceo
+
+task body Control_Velocidad_Alabeo
+
+end Control_Velocidad_Alabeo
+
+task body Control_Velocidad_CabeceoAlabeo
+
+end Control_Velocidad_CabeceoAlabeo
+
+task body Control_Potencia_Motor
+
+end Control_Potencia_Motor
+
+task body Control_Velocidad_Avion
+
+end Control_Velocidad_Avion
+
+end Control_Velocidad_Aeronave;
