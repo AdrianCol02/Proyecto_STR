37abf21f5c0d2cc59bfef23a61c9904c639941bc
AdrianCol02 <adrian.colugri@gmail.com>
2024-11-03 14:47:01 +0100

    Creados paquetes de pruebas para comprobar el perfecto funcionamiento de los sistemas del avión.
    Se crea una seción "main" provisional para realizar las pruebas.

---
+++ FSS-v1/pruebas_control_aeronave.ads
@@ -0,0 +1,15 @@
+-- pruebas_control_aeronave.ads
+with Datos_Aeronave;
+
+package pruebas_control_aeronave is
+
+    -- Procedimiento para probar el Escenario 1: Ajuste de Altitud en Condiciones Críticas
+    procedure Escenario_1;
+
+    -- Procedimiento para probar el Escenario 2: Corrección de Velocidad en Vuelo Rápido y Control de Alabeo
+    procedure Escenario_2;
+
+    -- Procedimiento para probar el Escenario 3: Respuesta a Baja Velocidad con Pitch Positivo
+    procedure Escenario_3;
+
+end pruebas_control_aeronave;
