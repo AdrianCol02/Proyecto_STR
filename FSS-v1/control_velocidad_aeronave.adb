2b6b4d91dbf9a96b13588036f03d38acf99bd9da
AdrianCol02 <adrian.colugri@gmail.com>
2024-11-28 19:10:18 +0100

    Updates: Se ha añadido el código necesario para realizar los test WCET
    TO DO:
    Realizar estudios WCET.
    Resolver incidencias.

--- FSS-v1/control_velocidad_aeronave.adb
+++ FSS-v1/control_velocidad_aeronave.adb
@@ -5,6 +5,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 with devicesFSS_V1; use devicesFSS_V1;
 with datos_posalt_vel;
 with datos_aeronave;
+with Ada.Execution_Time; use Ada.Execution_Time;
 
 package body control_velocidad_aeronave is
 
@@ -15,17 +16,19 @@ package body control_velocidad_aeronave is
         Velocidad_Actual : Speed_Samples_Type;
         Pitch_Actual : Pitch_Samples_Type;
         Roll_Actual : Roll_Samples_Type;
+        Start_Time, End_Time : CPU_Time;
+        WCET : CPU_Time := To_CPU_Time(0);
     begin
         loop
-           
-           Display_Speed(Velocidad_Actual);
+            Start_Time := Clock;
+
+            Display_Speed(Velocidad_Actual);
             -- Primero, leer la potencia del potenciometro y actualizar la velocidad
             Read_Power(Potenciometro);
             datos_aeronave.aeronave.Actualizar_Potencia(Potenciometro);
             Velocidad_Potencia := Float(Potenciometro) * Factor_Potenciometro;
 
             -- Limitar la velocidad calculada al rango permitido
-            
             if Velocidad_Potencia > Float(Max_Velocidad) then
                 Nueva_Velocidad := Max_Velocidad;
                 Light_2(On);
@@ -45,58 +48,13 @@ package body control_velocidad_aeronave is
             datos_posalt_vel.Datos_Vuelo.Leer_Pitch(Pitch_Actual);
             datos_posalt_vel.Datos_Vuelo.Leer_Roll(Roll_Actual);
 
-            -- Control de velocidad por Cabeceo (Pitch)
-            if Pitch_Actual > 0 and then Velocidad_Actual < Max_Velocidad then
-                Velocidad_Actual := Velocidad_Actual + Incremento_Cabeceo;
-                if Velocidad_Actual > Max_Velocidad then
-                    Velocidad_Actual := Max_Velocidad;
-                    Light_2(On);
-                end if;
-                Set_Speed(Velocidad_Actual);
-                datos_aeronave.aeronave.Actualizar_Velocidad(Velocidad_Actual);
-                datos_posalt_vel.Datos_Vuelo.Actualizar_Velocidad(Velocidad_Actual);
+            End_Time := Clock;
+            if End_Time - Start_Time > WCET then
+                WCET := End_Time - Start_Time;
             end if;
 
-            -- Control de velocidad por Alabeo (Roll)
-            if Roll_Actual > 0 and then Velocidad_Actual < Max_Velocidad then
-                Velocidad_Actual := Velocidad_Actual + Incremento_Alabeo;
-                if Velocidad_Actual > Max_Velocidad then
-                    Velocidad_Actual := Max_Velocidad;
-                    Light_2(On);
-                end if;
-                Set_Speed(Velocidad_Actual);
-                datos_aeronave.aeronave.Actualizar_Velocidad(Velocidad_Actual);
-                datos_posalt_vel.Datos_Vuelo.Actualizar_Velocidad(Velocidad_Actual);
-            end if;
-
-            -- Control de velocidad por Cabeceo y Alabeo combinados
-            if Pitch_Actual > 0 and then Roll_Actual > 0 and then Velocidad_Actual < Max_Velocidad then
-                Velocidad_Actual := Velocidad_Actual + Incremento_CabeceoAlabeo;
-                if Velocidad_Actual > Max_Velocidad then
-                    Velocidad_Actual := Max_Velocidad;
-                    Light_2(On);
-                end if;
-                Set_Speed(Velocidad_Actual);
-                datos_aeronave.aeronave.Actualizar_Velocidad(Velocidad_Actual);
-                datos_posalt_vel.Datos_Vuelo.Actualizar_Velocidad(Velocidad_Actual);
-            end if;
-
-            -- Control de potencia del motor y alarmas si la velocidad es baja
-            if Velocidad_Actual < Min_Velocidad then
-                Light_2(On);
-                Alarm(3);
-            end if;
-
-            -- Ajuste de velocidad en caso de alcanzar la velocidad crítica
-            if Velocidad_Actual < Velocidad_Critica then
-                Alarm(5);
-                Set_Speed(Min_Velocidad);
-                datos_aeronave.aeronave.Actualizar_Velocidad(Min_Velocidad);
-                datos_posalt_vel.Datos_Vuelo.Actualizar_Velocidad(Min_Velocidad);
-            end if;
-            Display_Speed(Velocidad_Actual);
-
-            -- Espera hasta la siguiente iteración
+            -- Mostrar el WCET
+            Ada.Text_IO.Put_Line("WCET: " & CPU_Time'Image(WCET));
             delay until Clock + Intervalo_Control;
         end loop;
     end control_velocidad;
